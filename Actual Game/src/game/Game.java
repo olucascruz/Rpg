479415ba856d8c5681c9dfca73585073446aecbf
LarissaRC <50839355+LarissaRC@users.noreply.github.com>
2022-10-17 10:54:57 -0400

    Ajuste dos métodos de desenhar

--- Actual Game/src/game/Game.java
+++ Actual Game/src/game/Game.java
@@ -135,97 +135,37 @@ public class Game extends Canvas implements Runnable, KeyListener{
 		
 		if(mago_ativo) {
 			// Floresta de Noite
-			DrawImg(g,mago.getBackground(),
-					0,
-					0,
-					700,
-					500);
+			DrawImg(g,mago.getBackground(), 0, 0, 700, 500);
 			// Mago
-			DrawImg(g, mago.getImage(),
-					mago.getPosition_x(),
-					mago.getPosition_y(),
-					600,
-					420);
+			DrawImg(g, mago.getImage(), mago.getPosition_x(), mago.getPosition_y(), 600, 420);
 			
 			// Vidas
 			if(mago.getVida() == 3) {
-				DrawImg(g, "img/coracao_cheio.png",
-						480,
-						-20,
-						600,
-						420);
-				DrawImg(g, "img/coracao_cheio.png",
-						540,
-						-20,
-						600,
-						420);
-				DrawImg(g, "img/coracao_cheio.png",
-						600,
-						-20,
-						600,
-						420);
+				DrawImg(g, "img/coracao_cheio.png", 480, -20, 600, 420);
+				DrawImg(g, "img/coracao_cheio.png", 540, -20, 600, 420);
+				DrawImg(g, "img/coracao_cheio.png", 600, -20, 600, 420);
 			} else if(mago.getVida() == 2){
-				DrawImg(g, "img/coracao_cheio.png",
-						480,
-						-20,
-						600,
-						420);
-				DrawImg(g, "img/coracao_cheio.png",
-						540,
-						-20,
-						600,
-						420);
-				DrawImg(g, "img/coracao_vazio.png",
-						600,
-						-20,
-						600,
-						420);
+				DrawImg(g, "img/coracao_cheio.png", 480, -20, 600, 420);
+				DrawImg(g, "img/coracao_cheio.png", 540, -20,600, 420);
+				DrawImg(g, "img/coracao_vazio.png", 600, -20, 600, 420);
 			} else if(mago.getVida() == 1) {
-				DrawImg(g, "img/coracao_cheio.png",
-						480,
-						-20,
-						600,
-						420);
-				DrawImg(g, "img/coracao_vazio.png",
-						540,
-						-20,
-						600,
-						420);
-				DrawImg(g, "img/coracao_vazio.png",
-						600,
-						-20,
-						600,
-						420);
+				DrawImg(g, "img/coracao_cheio.png",  480, -20, 600, 420);
+				DrawImg(g, "img/coracao_vazio.png", 540, -20, 600, 420);
+				DrawImg(g, "img/coracao_vazio.png", 600, -20, 600, 420);
 			}
 			/*
 			// Rapidez
-			DrawImg(g, mago.getRapidez(),
-					30,
-					40,
-					600,
-					420);
+			DrawImg(g, mago.getRapidez(), 30, 40, 600, 420);
 			
 			// Invis�vel
-			DrawImg(g, mago.getInvisivel(),
-					mago.getPosition_x(),
-					mago.getPosition_y(),
-					600,
-					420);
+			DrawImg(g, mago.getInvisivel(), mago.getPosition_x(), mago.getPosition_y(), 600, 420);
 			*/
 		} else if(dragao_ativo) {
 			// Deserto
-			DrawImg(g, dragao.getBackground(),
-					0,
-					0,
-					700,
-					500);
+			DrawImg(g, dragao.getBackground(), 0, 0, 700, 500);
 			
 			// Drag�o
-			DrawImg(g, dragao.getImage(),
-					dragao.getPosition_x(),
-					dragao.getPosition_y(),
-					600,
-					420);
+			DrawImg(g, dragao.getImage(), dragao.getPosition_x(), dragao.getPosition_y(), 600, 420);
 			
 			// Vidas
 			if(dragao.getVida() == 3) {
@@ -233,73 +173,31 @@ public class Game extends Canvas implements Runnable, KeyListener{
 				DrawImg(g, "img/coracao_cheio.png", 540, -60, 600, 420);
 				DrawImg(g, "img/coracao_cheio.png", 600, -60, 600, 420);
 			} else if(dragao.getVida() == 2){
-				DrawImg(g, "img/coracao_cheio.png",
-						480,
-						-60,
-						600,
-						420);
-				DrawImg(g, "img/coracao_cheio.png",
-						540,
-						-60,
-						600,
-						420);
-				DrawImg(g, "img/coracao_vazio.png",
-						600,
-						-60,
-						600,
-						420);
+				DrawImg(g, "img/coracao_cheio.png", 480, -60, 600, 420);
+				DrawImg(g, "img/coracao_cheio.png", 540, -60, 600, 420);
+				DrawImg(g, "img/coracao_vazio.png", 600, -60, 600, 420);
 			} else if(dragao.getVida() == 1) {
-				DrawImg(g, "img/coracao_cheio.png",
-						480,
-						-60,
-						600,
-						420);
-				DrawImg(g, "img/coracao_vazio.png",
-						540,
-						-60,
-						600,
-						420);
-				DrawImg(g, "img/coracao_vazio.png",
-						600,
-						-60,
-						600,
-						420);
+				DrawImg(g, "img/coracao_cheio.png", 480, -60, 600, 420);
+				DrawImg(g, "img/coracao_vazio.png", 540, -60, 600, 420);
+				DrawImg(g, "img/coracao_vazio.png", 600, -60, 600, 420);
 			}
 			/*
 			// Mordida
-			DrawImg(g, dragao.getMordida(),
-					30,
-					20,
-					600,
-					420);
+			DrawImg(g, dragao.getMordida(), 30, 20, 600, 420);
 			
 			// Rajada de fogo
-			DrawImg(g, dragao.getFogo(),
-					30,
-					20,
-					600,
-					420);
+			DrawImg(g, dragao.getFogo(), 30, 20, 600, 420);
 			*/
 		} else if(pocao_ativo) {
 			// Floresta de dia
-			DrawImg(g, cavaleiro.getBackground(),
-					0,
-					0,
-					700,
-					500);
+			DrawImg(g, cavaleiro.getBackground(), 0, 0, 700, 500);
 			
 			// Po��o
-			DrawImg(g, "img/pocao.png",
-					100,
-					50,
-					600,
-					450);
+			DrawImg(g, "img/pocao.png", 100, 50, 600, 450);
 		}
 		
 		// Cavaleiro
-		DrawImg(g, cavaleiro.getImagePlayer(), cavaleiro.getPosition_x(), cavaleiro.getPosition_y(),
-				600,
-				420);
+		DrawImg(g, cavaleiro.getImagePlayer(), cavaleiro.getPosition_x(), cavaleiro.getPosition_y(), 600, 420);
 	
 		// Vidas
 		if(cavaleiro.getVida() == 3) {
@@ -308,120 +206,52 @@ public class Game extends Canvas implements Runnable, KeyListener{
 			DrawImg(g, "img/coracao_cheio.png", 110, 40, 600, 420);
 		} else if(cavaleiro.getVida() == 2){
 			DrawImg(g, "img/coracao_cheio.png", -10, 40, 600, 420);
-			DrawImg(g, "img/coracao_cheio.png",
-					50,
-					40,
-					600,
-					420);
-			DrawImg(g, "img/coracao_vazio.png",
-					110,
-					40,
-					600,
-					420);
+			DrawImg(g, "img/coracao_cheio.png", 50, 40, 600, 420);
+			DrawImg(g, "img/coracao_vazio.png", 110, 40, 600, 420);
 		} else if(cavaleiro.getVida() == 1) {
-			DrawImg(g, "img/coracao_cheio.png",
-					-10,
-					40,
-					600,
-					420);
-			DrawImg(g, "img/coracao_vazio.png",
-					50,
-					40,
-					600,
-					420);
-			DrawImg(g, "img/coracao_vazio.png",
-					110,
-					40,
-					600,
-					420);
+			DrawImg(g, "img/coracao_cheio.png", -10, 40, 600, 420);
+			DrawImg(g, "img/coracao_vazio.png", 50, 40, 600, 420);
+			DrawImg(g, "img/coracao_vazio.png", 110, 40, 600, 420);
 		}
 		/*
 		// Atacar
-		DrawImg(g, cavaleiro.getAtacar(),
-				30,
-				20,
-				600,
-				420);
+		DrawImg(g, cavaleiro.getAtacar(), 30, 20, 600, 420);
 	
 		// Defender
-		DrawImg(g, cavaleiro.getDefender(),
-				30,
-				20,
-				600,
-				420);
+		DrawImg(g, cavaleiro.getDefender(), 30, 20, 600, 420);
 		*/
 		
 		// Troca de cenas - stand by
 		if(cena_stand_by) {
-			DrawImg(g, updateCenaStandBy(cena_atual),
-					0,
-					0,
-					700,
-					500);
+			DrawImg(g, updateCenaStandBy(cena_atual), 0, 0, 700, 500);
 		}
 		
 		// Op��es do Cavaleiro
 		if(cavaleiro_opcoes) {
 			// Se for a cena da po��o
 			if(cena_atual == 0) {
-				DrawImg(g, "img/cavaleiro_opcoes_1.png",
-						0,
-						0,
-						700,
-						500);
+				DrawImg(g, "img/cavaleiro_opcoes_1.png", 0, 0, 700, 500);
 				// Sublinhados
 				if(cavaleiro_opcao == 0) {
 					DrawImg(g, "img/sublinhado.png", 40, 0, 700, 500);
 				} else if(cavaleiro_opcao == 1) {
-					DrawImg(g, "img/sublinhado.png",
-							220,
-							0,
-							700,
-							500);
+					DrawImg(g, "img/sublinhado.png", 220, 0, 700, 500);
 				} else {
-					DrawImg(g, "img/sublinhado.png",
-							400,
-							0,
-							700,
-							500);
+					DrawImg(g, "img/sublinhado.png", 400, 0, 700, 500);
 				}
 			} else {
-				DrawImg(g, "img/cavaleiro_opcoes_2.png",
-						0,
-						0,
-						700,
-						500);
+				DrawImg(g, "img/cavaleiro_opcoes_2.png", 0, 0, 700, 500);
 				// Sublinhados
 				if(cavaleiro_opcao == 0) {
-					DrawImg(g, "img/sublinhado.png",
-							0,
-							0,
-							700,
-							500);
+					DrawImg(g, "img/sublinhado.png", 0, 0, 700, 500);
 				} else if(cavaleiro_opcao == 1) {
-					DrawImg(g, "img/sublinhado.png",
-							120,
-							0,
-							700,
-							500);
+					DrawImg(g, "img/sublinhado.png", 120, 0, 700, 500);
 				} else if(cavaleiro_opcao == 2){
-					DrawImg(g, "img/sublinhado.png",
-							230,
-							0,
-							700,
-							500);
+					DrawImg(g, "img/sublinhado.png", 230, 0, 700, 500);
 				} else if(cavaleiro_opcao == 3){
-					DrawImg(g, "img/sublinhado.png",
-							330,
-							0,
-							700,
-							500);
+					DrawImg(g, "img/sublinhado.png", 330, 0, 700, 500);
 				} else if(cavaleiro_opcao == 4){
-					DrawImg(g, "img/sublinhado.png",
-							430,
-							0,
-							700,
-							500);
+					DrawImg(g, "img/sublinhado.png", 430, 0, 700, 500);
 				}
 			}
 		}
@@ -468,7 +298,7 @@ public class Game extends Canvas implements Runnable, KeyListener{
 				cavaleiro_opcoes = true;
 			} else if(cavaleiro_opcoes) {
 				updateEncontro();
-				controlarAcoesDosPersonagens(cavaleiro, "Atacar");
+				//controlarAcoesDosPersonagens(cavaleiro, "Atacar");
 				cena_stand_by = true;
 				cavaleiro_opcoes = false;
 				cavaleiro_opcao = 0;
--- Actual Game/src/game/Main.java
+++ Actual Game/src/game/Main.java
@@ -7,29 +7,25 @@ import javax.swing.JFrame;
 import Model.Cavaleiro;
 import Model.Dragao;
 import Model.Mago;
-import game.Game;
-import game.musicStuff;
-
 
 public class Main {
-	public static void main(String[] args){
-		
+	public static void main(String[] args) {
+
 		// Personagens
 		Cavaleiro cavaleiro = new Cavaleiro("XXLucazXX", 10, 70);
 		Mago mago = new Mago("Felix", 100, 60);
 		Dragao dragao = new Dragao("Bruto", 100, 30);
 
 		Game game = new Game();
-		JFrame frame = new JFrame("Space Capivara");
-		String filePath = "spaceThreat.wav";
+		JFrame frame = new JFrame("Sopro de ogo");
+		String filePath = "game_song.wav";
 		musicStuff musicObject = new musicStuff();
-		
-		
+
 		musicObject.playMusic(filePath);
 		frame.add(game);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
